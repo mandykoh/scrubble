1e0d0a1e18e06917be7ceffbfef17ed391c0d377
Amanda Koh <amanda@naucera.net>
2018-03-12 08:50:21 +1100

    Add constructors for Games.

--- game.go
+++ game.go
@@ -6,3 +6,31 @@ type Game struct {
 	Bag   Bag
 	Board Board
 }
+
+// GameWithDefaults returns a game with a default board layout and bag setup,
+// with the specified players.
+func GameWithDefaults(p1 *Player, p2 *Player, others ...*Player) Game {
+	return GameWithSettings(
+		BagWithStandardEnglishTiles(),
+		BoardWithStandardLayout(),
+		p1, p2, others...)
+}
+
+// GameWithSettings returns a game set up with seats for the given players,
+// using the specified bag and board.
+func GameWithSettings(bag Bag, board Board, p1 *Player, p2 *Player, others ...*Player) Game {
+	seats := []Seat{
+		{OccupiedBy: p1},
+		{OccupiedBy: p2},
+	}
+
+	for _, p := range others {
+		seats = append(seats, Seat{OccupiedBy: p})
+	}
+
+	return Game{
+		Seats: seats,
+		Bag:   bag,
+		Board: board,
+	}
+}
