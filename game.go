6daad97116b851c16b2dac74e0f685cf5584c8f3
Amanda Koh <amanda@naucera.net>
2018-03-14 22:15:51 +1100

    Implement starting a game.

--- game.go
+++ game.go
@@ -1,12 +1,15 @@
 package scrubble
 
+import "math/rand"
+
 // Game represents the rules and simulation for a single game. The zero-value of
 // a Game is a game in the SetupPhase.
 type Game struct {
-	Phase GamePhase
-	Seats []Seat
-	Bag   Bag
-	Board Board
+	Phase            GamePhase
+	Seats            []Seat
+	Bag              Bag
+	Board            Board
+	CurrentSeatIndex int
 }
 
 // AddPlayer adds a seat for a new player to the game.
@@ -23,4 +26,21 @@ func (g *Game) RemovePlayer(p *Player) {
 			break
 		}
 	}
+}
+
+// Start begins the game by shuffling the bag, picking a random seat for the
+// first turn, filling all players' tile racks from the bag, and moving the game
+// into the MainPhase.
+//
+// The supplied random number generator is used to determine the bag shuffling
+// and the starting player.
+func (g *Game) Start(r *rand.Rand) {
+	g.CurrentSeatIndex = r.Intn(len(g.Seats))
+	g.Bag.Shuffle(r)
+
+	for i := range g.Seats {
+		g.Seats[i].Rack.FillFromBag(&g.Bag)
+	}
+
+	g.Phase = MainPhase
 }
--- game_test.go
+++ game_test.go
@@ -1,6 +1,10 @@
 package scrubble
 
-import "testing"
+import (
+	"math/rand"
+	"testing"
+	"time"
+)
 
 func TestGame(t *testing.T) {
 
@@ -104,6 +108,71 @@ func TestGame(t *testing.T) {
 			}
 			if actual, expected := game.Seats[1].OccupiedBy, p2; actual != expected {
 				t.Errorf("Expected seat to still be occupied by player %s but was %+v", expected.Name, actual)
+			}
+		})
+	})
+
+	t.Run(".Start()", func(t *testing.T) {
+		seed := time.Now().UnixNano()
+
+		expectedRand := rand.New(rand.NewSource(seed))
+		expectedStartingSeat := expectedRand.Intn(3)
+
+		expectedBag := BagWithStandardEnglishTiles()
+		expectedBag.Shuffle(expectedRand)
+
+		var expectedRacks []Rack
+		for i := 0; i < 3; i++ {
+			var rack Rack
+			for j := 0; j < MaxRackTiles; j++ {
+				rack = append(rack, expectedBag.DrawTile())
+			}
+			expectedRacks = append(expectedRacks, rack)
+		}
+
+		p1 := &Player{"Alice"}
+		p2 := &Player{"Bob"}
+		p3 := &Player{"Carol"}
+
+		game := Game{
+			Bag:   BagWithStandardEnglishTiles(),
+			Board: BoardWithStandardLayout(),
+		}
+		game.AddPlayer(p1)
+		game.AddPlayer(p2)
+		game.AddPlayer(p3)
+		game.Start(rand.New(rand.NewSource(seed)))
+
+		t.Run("sets the phase to Main", func(t *testing.T) {
+			if actual, expected := game.Phase, MainPhase; actual != expected {
+				t.Errorf("Expected game to be in %s phase but was %s", expected, actual)
+			}
+		})
+
+		t.Run("picks a random starting seat", func(t *testing.T) {
+			if actual, expected := game.CurrentSeatIndex, expectedStartingSeat; actual != expected {
+				t.Errorf("Expected starting seat to be %d but was %d", expected, actual)
+			}
+		})
+
+		t.Run("shuffles the bag", func(t *testing.T) {
+			for i, expected := range expectedBag {
+				if actual := game.Bag[i]; actual != expected {
+					t.Errorf("Expected tile %d to be %c(%d) but was %c(%d)", i, expected.Letter, expected.Points, actual.Letter, actual.Points)
+				}
+			}
+		})
+
+		t.Run("fills the players' racks", func(t *testing.T) {
+			for i := 0; i < 3; i++ {
+				if actual, expected := len(game.Seats[i].Rack), len(expectedRacks[i]); actual != expected {
+					t.Fatalf("Expected player '%s' to have a full rack of %d tiles but found %d", game.Seats[i].OccupiedBy.Name, expected, actual)
+				}
+				for j, expected := range expectedRacks[i] {
+					if actual := game.Seats[i].Rack[j]; actual != expected {
+						t.Errorf("Expected tile %d of %s's rack to be %c(%d) but was %c(%d)", j, game.Seats[i].OccupiedBy.Name, expected.Letter, expected.Points, actual.Letter, actual.Points)
+					}
+				}
 			}
 		})
 	})
