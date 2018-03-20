1c2fd724e838dbce8cbf146c767da5ce0a4e5551
Amanda Koh <amanda@naucera.net>
2018-03-20 18:42:01 +1100

    More neatly encapsulate phase checking.

--- game.go
+++ game.go
@@ -14,29 +14,22 @@ type Game struct {
 
 // AddPlayer adds a seat for a new player to the game.
 func (g *Game) AddPlayer(p *Player) error {
-	if g.Phase != SetupPhase {
-		return GameOutOfPhaseError{SetupPhase, g.Phase}
-	}
-
-	g.Seats = append(g.Seats, Seat{OccupiedBy: p})
-	return nil
+	return g.requirePhase(SetupPhase, func() {
+		g.Seats = append(g.Seats, Seat{OccupiedBy: p})
+	})
 }
 
 // RemovePlayer removes the seat occupied by the specified player. If no such
 // seat exists, this has no effect.
 func (g *Game) RemovePlayer(p *Player) error {
-	if g.Phase != SetupPhase {
-		return GameOutOfPhaseError{SetupPhase, g.Phase}
-	}
-
-	for i, s := range g.Seats {
-		if s.OccupiedBy == p {
-			g.Seats = append(g.Seats[:i], g.Seats[i+1:]...)
-			break
+	return g.requirePhase(SetupPhase, func() {
+		for i, s := range g.Seats {
+			if s.OccupiedBy == p {
+				g.Seats = append(g.Seats[:i], g.Seats[i+1:]...)
+				break
+			}
 		}
-	}
-
-	return nil
+	})
 }
 
 // Start begins the game by shuffling the bag, picking a random seat for the
@@ -46,17 +39,23 @@ func (g *Game) RemovePlayer(p *Player) error {
 // The supplied random number generator is used to determine the bag shuffling
 // and the starting player.
 func (g *Game) Start(r *rand.Rand) error {
-	if g.Phase != SetupPhase {
-		return GameOutOfPhaseError{SetupPhase, g.Phase}
+	return g.requirePhase(SetupPhase, func() {
+		g.CurrentSeatIndex = r.Intn(len(g.Seats))
+		g.Bag.Shuffle(r)
+
+		for i := range g.Seats {
+			g.Seats[i].Rack.FillFromBag(&g.Bag)
+		}
+
+		g.Phase = MainPhase
+	})
+}
+
+func (g *Game) requirePhase(phase GamePhase, action func()) error {
+	if g.Phase != phase {
+		return GameOutOfPhaseError{phase, g.Phase}
 	}
 
-	g.CurrentSeatIndex = r.Intn(len(g.Seats))
-	g.Bag.Shuffle(r)
-
-	for i := range g.Seats {
-		g.Seats[i].Rack.FillFromBag(&g.Bag)
-	}
-
-	g.Phase = MainPhase
+	action()
 	return nil
 }
