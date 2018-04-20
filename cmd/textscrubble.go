792cff2b3b3df08a2a20c3a2c985b8b1612fd43b
Amanda Koh <amanda@naucera.net>
2018-04-20 22:37:37 +1000

    Initial implementation of textscrubble.

---
+++ cmd/textscrubble.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"os"
+
+	"time"
+
+	"fmt"
+	"math/rand"
+
+	"bufio"
+
+	"regexp"
+
+	"strings"
+
+	gt "github.com/buger/goterm"
+	"github.com/mandykoh/scrubble"
+	"github.com/mandykoh/scrubble/cmd/textscrubble"
+)
+
+func lettersToRackTiles(letters []string, rack scrubble.Rack) (tiles []scrubble.Tile) {
+
+LetterSearch:
+	for _, l := range letters {
+		letter := []rune(l)[0]
+
+		for _, t := range rack {
+			if t.Letter == letter {
+				tiles = append(tiles, t)
+				continue LetterSearch
+			}
+		}
+
+		tiles = append(tiles, scrubble.Tile{Letter: letter, Points: 1})
+	}
+	return
+}
+
+func main() {
+	cmdExchangePattern := regexp.MustCompile(`^exchange ([a-zA-Z]+)$`)
+
+	rng := rand.New(rand.NewSource(time.Now().UnixNano()))
+	game := &scrubble.Game{
+		Board: scrubble.BoardWithStandardLayout(),
+		Bag:   scrubble.BagWithStandardEnglishTiles(),
+	}
+
+	for i := 1; i < len(os.Args); i++ {
+		player := &scrubble.Player{Name: os.Args[i]}
+		game.AddPlayer(player)
+	}
+
+	err := game.Start(rng)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error starting game: %v\n", err)
+		os.Exit(1)
+	}
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		seat := game.CurrentSeat()
+		textscrubble.DrawGame(game)
+
+		gt.Println()
+
+		if game.Phase == scrubble.EndPhase {
+			break
+		}
+
+		scanner.Scan()
+		line := scanner.Text()
+
+		if line == "rack" {
+			textscrubble.DrawRack(seat.Rack)
+
+		} else if line == "pass" {
+			err := game.Pass()
+			if err != nil {
+				gt.Println(gt.Color(err.Error(), gt.RED))
+			}
+
+		} else if matches := cmdExchangePattern.FindStringSubmatch(line); matches != nil {
+			lettersToExchange := strings.Split(matches[1], "")
+			tiles := lettersToRackTiles(lettersToExchange, seat.Rack)
+			err := game.ExchangeTiles(tiles, rng)
+			if err != nil {
+				gt.Println(gt.Color(err.Error(), gt.RED))
+			} else {
+				textscrubble.DrawRack(seat.Rack)
+				gt.Printf("\n%s exchanged tiles", seat.OccupiedBy.Name)
+			}
+
+		} else if line == "?" {
+			gt.Println("      rack - show rack")
+			gt.Println("    across - play tiles across, eg: across 1 3 DOG")
+			gt.Println("      down - play tiles down, eg: down 4 2 DOG")
+			gt.Println("      pass - forfeit turn")
+			gt.Println("  exchange - exchange tiles, eg: exchange DG")
+			gt.Println(" challenge - challenge the last play")
+		}
+	}
+}
---
+++ cmd/textscrubble/draw.go
@@ -0,0 +1,75 @@
+package textscrubble
+
+import (
+	gt "github.com/buger/goterm"
+	"github.com/mandykoh/scrubble"
+)
+
+func DrawBoard(b *scrubble.Board) {
+	_, st, dl, dw, tl, tw := scrubble.BoardPositionTypes()
+
+	for r := 0; r < b.Rows; r++ {
+		offsetY := r*2 + 1
+
+		for c := 0; c < b.Columns; c++ {
+			offsetX := c*4 + 1
+
+			gt.MoveCursor(offsetX, offsetY)
+			gt.Print(gt.Color(".", gt.GREEN))
+			gt.MoveCursorDown(1)
+			gt.MoveCursorBackward(1)
+			gt.Print(gt.Color("|", gt.GREEN))
+
+			pos := b.Position(scrubble.Coord{Row: r, Column: c})
+
+			gt.MoveCursor(offsetX+1, offsetY+1)
+			switch pos.Type {
+			case st:
+				gt.Print(gt.Color("★", gt.WHITE))
+			case dl:
+				gt.Print(gt.Color("dl", gt.BLUE))
+			case dw:
+				gt.Print(gt.Color("dw", gt.RED))
+			case tl:
+				gt.Print(gt.Color("tl", gt.GREEN))
+			case tw:
+				gt.Print(gt.Color("tw", gt.YELLOW))
+			}
+
+			//if pos.Tile == nil {
+			//}
+		}
+
+		gt.MoveCursor(b.Columns*4+1, offsetY)
+		gt.Printf(gt.Color("| %d", gt.GREEN), r)
+		gt.MoveCursor(b.Columns*4+1, offsetY+1)
+		gt.Print(gt.Color("|", gt.GREEN))
+	}
+
+	for i := 0; i < b.Rows; i++ {
+		gt.MoveCursor(i*4+2, b.Rows*2+1)
+		gt.Printf(gt.Color("%d", gt.GREEN), i)
+	}
+}
+
+func DrawGame(g *scrubble.Game) {
+	gt.Clear()
+	DrawBoard(&g.Board)
+
+	gt.Println()
+	gt.Println()
+
+	if g.Phase == scrubble.EndPhase {
+		gt.Println("Game over")
+	} else {
+		gt.Printf("%s’s turn:", g.CurrentSeat().OccupiedBy.Name)
+	}
+
+	gt.Flush()
+}
+
+func DrawRack(r scrubble.Rack) {
+	for _, t := range r {
+		gt.Printf(gt.Color(" [%c %d]", gt.WHITE), t.Letter, t.Points)
+	}
+}
