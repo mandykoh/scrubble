1798e0fa272094420d233d0d1626cf2df1177410
Amanda Koh <amanda@naucera.net>
2018-05-09 09:37:59 +1000

    Switch to go-bump for versioning.

--- version.go
+++ version.go
@@ -1,8 +1,8 @@
 package scrubble
 
-//go:generate go run vendor/github.com/mandykoh/go-version/main.go scrubble
+//go:generate go run vendor/github.com/mandykoh/go-bump/main.go scrubble
 
 const VersionMajor = "1"
 const VersionMinor = "1"
-const VersionRevision = "3"
-const Version = "1.1.3"
+const VersionRevision = "4"
+const Version = "1.1.4"
