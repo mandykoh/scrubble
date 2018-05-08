5a1d6f3dc2da2ce61029f1b015f5fbb922221c9d
Amanda Koh <amanda@naucera.net>
2018-05-08 22:09:20 +1000

    Add version constants.

---
+++ version.go
@@ -0,0 +1,8 @@
+package scrubble
+
+//go:generate go run vendor/github.com/mandykoh/go-version/main.go scrubble
+
+const VersionMajor = "1"
+const VersionMinor = "1"
+const VersionRevision = "2"
+const Version = "1.1.2"
