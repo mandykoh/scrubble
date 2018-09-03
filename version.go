e0ef749c3e1da00c8481495bd0de7351abb44626
Amanda Koh <amanda@naucera.net>
2018-09-03 18:07:41 +1000

    Bump version.

--- version.go
+++ version.go
@@ -2,7 +2,7 @@ package scrubble
 
 //go:generate go-bump scrubble
 
-const VersionMajor = "1"
+const VersionMajor = "v1"
 const VersionMinor = "1"
-const VersionRevision = "6"
-const Version = "1.1.6"
+const VersionRevision = "7"
+const Version = "v1.1.7"
